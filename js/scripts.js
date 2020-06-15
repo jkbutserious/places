63d86d54199b790f558289fd99edd86dbc9d10ec
Sara Kane <kane.sarak@gmail.com>
2020-06-15 11:15:54 -0700

    Add click functions to show attribues for each place when the place name is clicked on
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,23 @@ function Place(name, location, landmarks, year) {
 }
 
 let placeList = new PlaceList();
-let place = new Place("New York City", "New York", "Statue of Liberty", "2013");
+let place1 = new Place("New York City", "New York", "Statue of Liberty", "2013");
 let place2 = new Place("Seoul", "South Korea", "Changgyeonggung Palace", "2013");
-placeList.addPlace(place);
+placeList.addPlace(place1);
 placeList.addPlace(place2);
+
+// User Interface Logic
+$(document).ready(function() {
+  $("#place1").click(function() {
+    $("#place-attributes").append("<li>" + place1.name + "</li>");
+    $("#place-attributes").append("<li>" + place1.location + "</li>");
+    $("#place-attributes").append("<li>" + place1.landmarks + "</li>");
+    $("#place-attributes").append("<li>" + place1.year + "</li>");
+  });
+  $("#place2").click(function() {
+    $("#place-attributes").append("<li>" + place2.name + "</li>");
+    $("#place-attributes").append("<li>" + place2.location + "</li>");
+    $("#place-attributes").append("<li>" + place2.landmarks + "</li>");
+    $("#place-attributes").append("<li>" + place2.year + "</li>");
+  });
+});
