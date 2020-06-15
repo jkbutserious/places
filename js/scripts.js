5825906db47b6062263f37e586d72adafdcbf4af
Sara Kane <kane.sarak@gmail.com>
2020-06-15 10:24:38 -0700

    Add constructors for PlacesList and Place
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 // Business Logic
+function PlaceList() {
+  this.place = [];
+}
+
+function Place(name, location, landmarks, timeOfYear) {
+  this.name = name;
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+}
