a7c370c96faacae0e02a7f5495b3bf42e2015adc
Sara Kane <kane.sarak@gmail.com>
2020-06-15 10:33:00 -0700

    Add function to add place objects to PlaceList and create two places and add them to placeList
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,19 @@ function PlaceList() {
   this.place = [];
 }
 
-function Place(name, location, landmarks, timeOfYear) {
+PlaceList.prototype.addPlace = function(place) {
+  this.place.push(place);
+}
+
+function Place(name, location, landmarks, year) {
   this.name = name;
   this.location = location;
   this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
+  this.year = year;
 }
+
+let placeList = new PlaceList();
+let place = new Place("New York City", "New York", "Statue of Liberty", "2013");
+let place2 = new Place("Seoul", "South Korea", "Changgyeonggung Palace", "2013");
+placeList.addPlace(place);
+placeList.addPlace(place2);
